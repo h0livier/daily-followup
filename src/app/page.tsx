1c68fb3c85838880f98b2b5786a2ffd940b20e47
Hayot Olivier <olivier.hayot.dev@gmail.com>
2025-05-23 00:40:11 +0200

    fixed style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import CheckBoxList from "@/components/checkbox-list";
 export default function Home() {
 
   return (
-    <div className="m-5">
-      <button className="text-3xl pb-5">Suivi Daily</button>
+    <div className="min-h-screen overflow-x-hidden p-5">
+      <h1 className="text-3xl pb-5">Suivi Daily</h1>
       <CheckBoxList />
     </div>
   );
